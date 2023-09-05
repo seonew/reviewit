5de9095281df84b3b082612d2d5c0aad5143e5ce
seonew <seonew.888@gmail.com>
2023-09-06 01:27:49 +0900

    Added Mongoose for database interaction

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  webpack(config) {
+    config.experiments = { ...config.experiments, topLevelAwait: true };
+    return config;
+  },
   images: {
     dangerouslyAllowSVG: true,
     remotePatterns: [
