afefd7951815dc51a5451cdb70412e21ba86eb5e
seonew <seonew.888@gmail.com>
2024-02-05 23:28:40 +0900

    Added logic and API to update user data after login

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,16 +1,21 @@
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
+import dbConnect from "@/utils/db/mongodb";
+import UserModel from "@/models/user";
+
+const SERVICE = process.env.NEXT_PUBLIC_SERVICE ?? "";
+const LOGIN_URL = process.env.NEXT_PUBLIC_LOGIN_URL;
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const token = searchParams.get("token") ?? "";
-  const SERVICE = process.env.NEXT_PUBLIC_SERVICE ?? "";
-  const LOGIN_URL = process.env.NEXT_PUBLIC_LOGIN_URL;
 
   try {
     if (!token) {
       return NextResponse.json({ error: "Empty token", status: 401 });
     }
+
+    await dbConnect();
 
     const params = {
       token,
@@ -29,4 +34,39 @@ export async function GET(request: Request) {
   }
 
   return NextResponse.json({ error: "Internal Server Error", status: 500 });
+}
+
+export async function POST(request: Request) {
+  try {
+    const requestData = await request.json();
+    const { id, name, avatarUrl, loginType } = requestData;
+
+    await dbConnect();
+
+    const user = await UserModel.findOne({ id });
+    if (user === null) {
+      const newUser = new UserModel({
+        id,
+        name,
+        avatarUrl,
+        loginType,
+        loginService: SERVICE,
+        registerDate: new Date(),
+        updateDate: new Date(),
+      });
+      await newUser.save();
+    } else {
+      await user.updateOne({ updateDate: new Date() });
+    }
+
+    const result = {
+      id,
+      name: user.name,
+      avatarUrl: user.avatarUrl,
+      loginType,
+    };
+    return NextResponse.json(result);
+  } catch (error) {
+    console.log(error);
+  }
 }
