3327af31cc2d52ca0e227f3174ebf0809acfff0d
seonew <seonew.888@gmail.com>
2024-02-07 02:46:25 +0900

    Modified `/mypage` UI based on login status

--- app/mypage/list-page.tsx
+++ app/mypage/list-page.tsx
@@ -19,38 +19,48 @@ const List = () => {
     fetchLikedContents("product");
   }, [fetchLikedContents]);
 
+  const LikeList = () => {
+    return (
+      <>
+        <CardList
+          title={"My Book List"}
+          color={"text-gray-900"}
+          gridColsCSS={"grid-cols-4"}
+        >
+          {likedBooks &&
+            likedBooks.map((item: LikedContent) => {
+              return <CardInfo key={item.id} content={item} />;
+            })}
+        </CardList>
+        <CardList
+          title={"My Product List"}
+          color={"text-gray-900"}
+          gridColsCSS={"grid-cols-4"}
+        >
+          {likedProducts &&
+            likedProducts.map((item: LikedContent) => {
+              return <CardInfo key={item.id} content={item} />;
+            })}
+        </CardList>
+      </>
+    );
+  };
+
   return (
     <div className="contents-container">
-      <div className="flex items-start">
-        <div className="w-1/4 min-w-200">
-          {user.name && (
+      {user.name ? (
+        <div className="flex items-start">
+          <div className="w-1/4 min-w-200">
             <UserInfo user={user} onClickSignOut={handleClickSignOut} />
-          )}
+          </div>
+          <div className="w-12"></div>
+          <div className="w-10/12 pt-6 pl-2 overflow-hidden">
+            <LikeList />
+          </div>
         </div>
-        <div className="w-12"></div>
-        <div className="w-10/12 pt-6 pl-2 overflow-hidden">
-          <CardList
-            title={"My Book List"}
-            color={"text-gray-900"}
-            gridColsCSS={"grid-cols-4"}
-          >
-            {likedBooks &&
-              likedBooks.map((item: LikedContent) => {
-                return <CardInfo key={item.id} content={item} />;
-              })}
-          </CardList>
-          <CardList
-            title={"My Product List"}
-            color={"text-gray-900"}
-            gridColsCSS={"grid-cols-4"}
-          >
-            {likedProducts &&
-              likedProducts.map((item: LikedContent) => {
-                return <CardInfo key={item.id} content={item} />;
-              })}
-          </CardList>
-        </div>
-      </div>
+      ) : (
+        <LikeList />
+      )}
     </div>
   );
 };
