13430a99d8bed1d9543d82707f195ed40ec64802
seonew <seonew.888@gmail.com>
2024-03-22 00:22:05 +0900

    Refactored code

--- app/components/modal/CommentModal.tsx
+++ app/components/modal/CommentModal.tsx
@@ -39,35 +39,33 @@ const CommentModal = ({ open, review, onClose, onSubmit }: Props) => {
 
   return (
     <Modal open={open}>
-      <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all">
-        <div className="bg-white px-4 pb-4 pt-5">
-          <div className="flex items-start">
-            <div className="p-4 text-left">
-              <div className="text-center ">
-                <span className="text-lg font-semibold leading-6 text-gray-900">
-                  리뷰 쓰기
-                </span>
-                <button
-                  className="flex justify-center absolute top-5 right-5"
-                  onClick={handleClickClose}
-                  ref={cancelButtonRef}
-                >
-                  <XMarkIcon className="h-7 w-7 font-bold" />
-                </button>
-              </div>
-              <div className="my-6">
-                <div className="relative mb-4 w-full">
-                  <div className="px-5">
-                    <div className="pt-4 pb-2">
-                      <PreferenceSection
-                        contentLike={review?.contentLike}
-                        onClick={handleSubmitContentLike}
-                      />
-                      <CommentTextEditor
-                        content={review?.content}
-                        onClick={handleSubmitReview}
-                      />
-                    </div>
+      <div className="bg-white px-4 pb-4 pt-5">
+        <div className="flex items-start">
+          <div className="p-4 text-left">
+            <div className="text-center ">
+              <span className="text-lg font-semibold leading-6 text-gray-900">
+                리뷰 쓰기
+              </span>
+              <button
+                className="flex justify-center absolute top-5 right-5"
+                onClick={handleClickClose}
+                ref={cancelButtonRef}
+              >
+                <XMarkIcon className="h-7 w-7 font-bold" />
+              </button>
+            </div>
+            <div className="my-6">
+              <div className="relative mb-4 w-full">
+                <div className="px-5">
+                  <div className="pt-4 pb-2">
+                    <PreferenceSection
+                      contentLike={review?.contentLike}
+                      onClick={handleSubmitContentLike}
+                    />
+                    <CommentTextEditor
+                      content={review?.content}
+                      onClick={handleSubmitReview}
+                    />
                   </div>
                 </div>
               </div>
