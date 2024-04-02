c3ba9c7eded8260199e9d8270856ad0da9000c57
seonew <seonew.888@gmail.com>
2024-04-03 00:06:18 +0900

    Changed to token authentication library

--- utils/error.ts
+++ utils/error.ts
@@ -1,13 +1,13 @@
-export class NotFoundUserError extends Error {
-  constructor() {
-    super();
-    this.name = "NotFoundUserError";
-  }
-}
-
 export class NotFoundContentError extends Error {
   constructor() {
     super();
     this.name = "NotFoundContentError";
+  }
+}
+
+export class UnauthorizedError extends Error {
+  constructor() {
+    super();
+    this.name = "UnauthorizedError";
   }
 }
